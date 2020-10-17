2a6d086b172bdd1d04f77062553b2309eec2c996
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-10-17 12:31:52 -0400

    removed several ascii chars from password character array and imported the background image

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     //the bricks for the wall
     const characterArrayPlainText = [
         "a","b","c","d","e","f","g","h","i","j",
-        "k","l","m","n","o","p","q","r","s","t",
-        "u","v","w","x","y","z","0","1","2","3",
+        "k","m","n","o","p","q","r","s","t",
+        "u","v","w","x","y","z","2","3",
         "4","5","6","7","8","9","A","B","C","D",
         "E","F","G","H","I","J","K","L","M","N",
-        "O","P","Q","R","S","T","U","V","W","X",
+        "P","Q","R","S","T","U","V","W","X",
         "Y","Z","!","@","#","$","%","^","&","*",
         "(",")",
     ];
